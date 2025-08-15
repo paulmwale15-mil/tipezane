6177ff6843f680eb364653b16f00c667d0db3c33
paulmwale15-mil <paulmwale15@gmail.com>
2025-08-16 01:13:43 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Skills from "./pages/Skills";
 import Transport from "./pages/Transport";
@@ -10,18 +8,12 @@ import RealEstate from "./pages/RealEstate";
 function App() {
   return (
     <Router>
-      <div className="min-h-screen flex flex-col">
-        <Navbar />
-        <main className="flex-1">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/skills" element={<Skills />} />
-            <Route path="/transport" element={<Transport />} />
-            <Route path="/real-estate" element={<RealEstate />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/transport" element={<Transport />} />
+        <Route path="/realestate" element={<RealEstate />} />
+      </Routes>
     </Router>
   );
 }
